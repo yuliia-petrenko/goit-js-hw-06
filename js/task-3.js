3bac4d1d1aea4390a4fc7243a66684fecb2ae4b7
yuliia-petrenko <mlyubit30@gmail.com>
2023-12-02 23:32:02 +0200

    add hw-6

---
+++ js/task-3.js
@@ -0,0 +1,26 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value;
+    this.#value += str;
+  }
+}
+const builder = new StringBuilder('.');
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
